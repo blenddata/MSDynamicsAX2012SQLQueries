96a42af456f9c9dd343bc619579376df45a8635b
Blend Data <blenddata@gmail.com>
2019-05-20 10:11:12 +0430

    Added view for all purchase orders receipts qty

---
+++ Database/Database/Views/Procurement and sourcing/Common/Purchase orders/All purchase orders receipts qty/AllPurchReceiptsQTY.sql
@@ -0,0 +1,11 @@
+﻿SELECT vpsj.PURCHID,
+       vpsj.DELIVERYDATE,
+       vpst.ITEMID,
+       SUM(vpst.ORDERED) ORDERED,
+       SUM(vpst.QTY) Received
+FROM dbo.VENDPACKINGSLIPJOUR vpsj
+    LEFT JOIN dbo.VENDPACKINGSLIPTRANS vpst
+        ON vpst.VENDPACKINGSLIPJOUR = vpsj.RECID
+GROUP BY vpsj.PURCHID,
+         vpsj.DELIVERYDATE,
+         vpst.ITEMID;
---
+++ Database/Database/Views/Procurement and sourcing/Common/Purchase orders/All purchase orders receipts qty/README.md
@@ -0,0 +1,9 @@
+﻿# Microsoft-Dynamics-AX-2012-SQL-Queries
+# Procurement and sourcing
+# Purchase orders
+
+Sum of all items qty in purchase order receipts.
+
+Step 1: Receipt line data: 
+
+		Aggregate qty by item: [done]
