1d16f58ad621bfe38d2e87cf84c4388411459279
Blend Data <blenddata@gmail.com>
2019-05-20 11:47:13 +0430

    Added view for all purchase orders confirmations

---
+++ Database/Database/Views/Procurement and sourcing/Common/Purchase orders/All purchase orders confirmations/AllPurchConfirm.sql
@@ -0,0 +1,31 @@
+﻿WITH cte
+AS (SELECT plav.PURCHID,
+           vpoj.PURCHORDERDATE,
+           plav.PURCHQTY,
+           plav.QTYORDERED,
+           plav.ITEMID,
+           ROW_NUMBER() OVER (PARTITION BY plav.PURCHID,
+                                           plav.ITEMID,
+                                           plav.PURCHQTY
+                              ORDER BY plav.ITEMID,
+                                       vpoj.PURCHORDERDATE DESC,
+                                       plav.PURCHQTY DESC
+                             ) ROWNO
+    FROM dbo.VENDPURCHORDERJOUR vpoj
+        LEFT JOIN dbo.PURCHTABLEALLVERSIONS ptav
+            ON ptav.PURCHTABLEVERSIONRECID = vpoj.PURCHTABLEVERSION
+        LEFT JOIN dbo.PURCHLINEALLVERSIONS plav
+            ON plav.PURCHTABLEVERSIONRECID = ptav.PURCHTABLEVERSIONRECID
+),
+     cte2
+AS (SELECT *,
+           ROW_NUMBER() OVER (PARTITION BY cte.PURCHID,
+                                           cte.ITEMID
+                              ORDER BY cte.PURCHORDERDATE DESC
+                             ) INROWNO
+    FROM cte
+    WHERE cte.ROWNO = 1
+          AND cte.PURCHQTY <> 0)
+SELECT *
+FROM cte2
+WHERE cte2.INROWNO = 1;
---
+++ Database/Database/Views/Procurement and sourcing/Common/Purchase orders/All purchase orders confirmations/README.md
@@ -0,0 +1,13 @@
+﻿### Microsoft Dynamics AX 2012 SQL Server Queries
+ 
+**Procurement and sourcing**
+
+**Purchase orders**
+
+*All purchase orders confirmations. We must fetch the last version of confirmation and some of `PURCHQTY` may be null or zero so we lookup for item qty in prev confirmations.*
+
+* Step 1: Purch table all version data: 
+	- [x] Fetch the last confirmation: `Done`
+
+* Step 1: Purch line table all version data: 
+	- [x] lookup for item qty in prev confirmations : `Done`
